427ecf751cdd277ae9fe1511a0a1530f9a35f943
Snehan Kekre <snehan.minerva@gmail.com>
2022-01-31 19:12:39 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-shap",
+    version="0.0.1",
+    author="Snehan Kekre",
+    author_email="snehan@streamlit.io",
+    description="Streamlit component for SHAP",
+    long_description="Streamlit component for SHAP (SHapley Additive exPlanations)",
+    long_description_content_type="text/plain",
+    url="https://github.com/snehankekre/streamlit-shap",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=["streamlit >= 1.4.0", "shap >= 0.4.0"],
+)
